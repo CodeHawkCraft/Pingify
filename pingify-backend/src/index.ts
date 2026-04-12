a006f3b405b66c5daa0b908b5a6090543cd7b19e
Davinder Kumar <davinder.kumar@intverse.io>
2026-04-12 18:55:14 +0530

    refactor: set express.json body size limit to 10kb

--- pingify-backend/src/index.ts
+++ pingify-backend/src/index.ts
@@ -8,7 +8,7 @@ import "./services/pusher.service.ts";
 const app = express();
 const PORT = env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
 app.use("/api", appRouter);
