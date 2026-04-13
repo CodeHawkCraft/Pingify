5185e429d822295172e59c3ff4bc1397155d5771
Davinder Kumar <davinder.kumar@intverse.io>
2026-04-14 00:12:24 +0530

    feat: add daisyUI

--- pingify-frontend/src/App.tsx
+++ pingify-frontend/src/App.tsx
@@ -1,7 +1,10 @@
 const App = () => {
   return (
-    <div className="bg-red-500">App</div>
-  )
-}
+    <div className="bg-red-500">
+      App
+      <button className="btn">Default</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
